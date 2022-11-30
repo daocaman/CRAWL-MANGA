b580e3b62c8f3233f45c8facdb517876721dea5e
daocaman <daocaman1998@gmail.com>
2022-11-30 10:11:09 +0700

    update archive file

--- ArchiveBulk.py
+++ ArchiveBulk.py
@@ -3,6 +3,6 @@ import shutil
 from icecream import ic
 
 for fol in os.listdir():
-    if os.path.isdir(fol) and "vol" in fol.lower():
+    if os.path.isdir(fol):
         ic("Archive " + fol + ".zip")
         shutil.make_archive(fol, "zip", base_dir=fol)
