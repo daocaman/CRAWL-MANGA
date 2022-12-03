b197aeca4c5fb84a07df9ae3b4211cae6cca4e74
daocaman <daocaman1998@gmail.com>
2022-12-03 20:07:27 +0700

    update download error img

--- downloadError.py
+++ downloadError.py
@@ -1,12 +1,18 @@
 import requests
+from icecream import ic
 
 links = []
 f = open("error.txt", "r", encoding="utf8")
 
+servers = {
+    "mangasee": "https://mangasee123.com/",
+    "nettruyen": "https://www.nettruyentv.com/",
+    "sinhvien": "https://kenhsinhvien.vn/"
+}
+
 
 errFile = open("error - new.txt", "w+", encoding="utf8")
 for x in f:
-    print(x)
     links.append(x)
 
 for img in links:
@@ -16,28 +22,24 @@ for img in links:
     tmp = ""
     if len(spe) != 2:
         tmp =  spe[0:-1]
-        print(tmp)
         tmp= " - ".join(tmp)
     else:
         tmp = spe[0]
     
 
     try:
-        print(spe[-1].replace("\n", ""))
-        r =requests.get(spe[-1].replace("\n", ""), headers={'User-agent': 'Mozilla/5.0', 'Referer': 'http://www.nettruyenpro.com/'})
-        # r = requests.get(spe[-1].replace("\n", ""), headers={'User-agent': 'Mozilla/5.0', 'Connection': 'keep-alive', 'Referer': 'https: // readmanganato.com /'})
+        link = spe[-1].replace("\n", "")
+        r =requests.get(link, headers={'User-agent': 'Mozilla/5.0', 'Referer': servers["nettruyen"]})
 
-        print(tmp)
+        ic(tmp)
         with open(tmp, "wb") as fd:
-
-            # fd.write(r.content)
-            print(r.status_code)
+            ic(r.status_code)
 
             if(r.status_code != 200):
                 errFile.write(img)
             else:
                 fd.write(r.content)
     except Exception as e:
-        print(e)
+        ic(e)
         errFile.write(img)
         continue
