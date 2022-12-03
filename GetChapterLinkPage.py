39a12ef1a0a76adb5081ac95573bc98b81d12379
daocaman <daocaman1998@gmail.com>
2022-12-03 20:16:58 +0700

    update get chapter link server nettruyen, mangaseee

--- GetChapterLinkPage.py
+++ GetChapterLinkPage.py
@@ -1,31 +1,43 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-link = 'https://mangasee123.com/manga/Pokmon-Movies-Manga'
+link = 'https://www.nettruyentv.com/truyen-tranh/truyen-nhan-atula-shura-no-mon-i-69010'
 
 driver = webdriver.Firefox(executable_path=r'./geckodriver.exe')
 
 driver.get(link)
 
-show_ele = driver.find_element(By.CLASS_NAME, "ShowAllChapters")
-
-if show_ele:
-    show_ele.click()
-
 target = []
 
-# nettruyen
-elements = driver.find_elements(By.CLASS_NAME, 'ChapterLink')
+# mode 1 nettruyen
+# mode 2 mangasee
 
-for ele in elements:
-    target.insert(0, ele.get_attribute('href'))
+mode = 1
 
+if mode == 1:
 
-f = open("link.txt", "w+")
-for i in target:
-    f.write(i + "\n")
+    keyword = "truyen-nhan-atula-shura-no-mon-i"
 
-f.close()
+    elements = driver.find_elements(By.CLASS_NAME, 'chapter')
+
+    for ele in elements:
+        find_ele = ele.find_element(By.TAG_NAME, 'a')
+        if keyword in find_ele.get_attribute('href'):
+            target.insert(0, find_ele.get_attribute('href'))
+
+    f = open("link.txt", "w+")
+    for i in target:
+        f.write(i + "\n")
+else:
+    show_ele = driver.find_element(By.CLASS_NAME, "ShowAllChapters")
+
+    if show_ele:
+        show_ele.click()
+
+    elements = driver.find_elements(By.CLASS_NAME, 'ChapterLink')
+
+    for ele in elements:
+        target.insert(0, ele.get_attribute('href'))
 
 driver.close()
 f.close()
