77df7e4e8420b6761514d89e58b56de59962e608
daocaman <daocaman1998@gmail.com>
2022-12-04 09:17:17 +0700

    update send cover

--- SendCover.py
+++ SendCover.py
@@ -7,9 +7,9 @@ def takeKey(elem):
     tmp = re.split(" |_",tmp)
     return int(tmp[len(tmp)-1])
 
-coverFolderTxt = "Duel master"
+coverFolderTxt = "Cover"
 
-prefix = "Duel Master"
+prefix = "xxxHOLIC"
 
 listFiles = os.listdir(coverFolderTxt)
 
@@ -17,14 +17,22 @@ listFiles = sorted(listFiles, key=takeKey)
 
 # print(listFiles)
 
+opt = 2
+
+# 1 make folder cover
+# 2 don't make folder
+
 for idx, cover in enumerate(listFiles):
 
     extFile = cover.split(".")[1]
 
     if not os.path.exists(prefix+" - Vol"+str(idx+1)):
         os.mkdir(prefix+" - Vol"+str(idx+1))
-    if not os.path.exists(prefix+" - Vol"+str(idx+1)+"/00_Cover"):
+    if not os.path.exists(prefix+" - Vol"+str(idx+1)+"/00_Cover") and opt == 1:
         os.mkdir(prefix+" - Vol"+str(idx+1)+"/00_Cover")
 
-    shutil.copyfile(coverFolderTxt+"/"+cover, prefix+" - Vol"+str(idx+1) +
-                    "/00_Cover"+"/"+"0000"+"."+extFile)
+        shutil.copyfile(coverFolderTxt+"/"+cover, prefix+" - Vol"+str(idx+1) +
+                        "/00_Cover"+"/"+"0000"+"."+extFile)
+    else:
+        shutil.copyfile(coverFolderTxt+"/"+cover, prefix+" - Vol"+str(idx+1) +
+                        "/"+"0000"+"."+extFile)
