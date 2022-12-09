fa71054b0d470754c83245329471d4f72036f593
daocaman <daocaman1998@gmail.com>
2022-12-09 17:23:49 +0700

    update gui download novel

--- CrawlManga.py
+++ CrawlManga.py
@@ -20,6 +20,43 @@ ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 
 n = ToastNotifier()
 
+common_color = {
+    "primiary": "color: #007bff;",
+    "success": "color: #28a745;",
+    "info": "color: #17a2b8;",
+    "warning": "color: #ffc107;",
+    "danger": "color: #dc3545;"
+}
+
+common_font = {
+    "bold": "font-weight: bold;",
+    "underline": "text-decoration: underline;"
+}
+
+font = {
+    "title": "font-size: 10pt;",
+    "mini_title": "font-size: 8pt;",
+    "main_title": "font-size: 16pt;"
+}
+
+tabs = {
+    "RN": "Rename Folder",
+    "GC": "Getting Chapter",
+    "DV": "Download Novel"
+}
+
+btns = {
+    "default": "border-radius: 6px; min-width: 80px; min-height: 35px;border-color: #007bff; border: 1px solid #007bff;",
+    "primary": "color: #fff;background-color: #007bff;border-color: #007bff;",
+    "success": "color: #fff;background-color: #28a745;border-color: #28a745;",
+    "info": "color: #fff;background-color: #17a2b8;border-color: #17a2b8;",
+    "danger": "color: #fff;background-color: #dc3545;border-color: #dc3545;",
+    "warning": "color: #fff;background-color: #ffc107;border-color: #ffc107;",
+}
+
+msg = {
+    "suc_rn": {"t": "Success", "m": "Rename folder done!!!"}
+}
 
 class CrawlManga(QWidget):
     def __init__(self, *args, **kwargs):
@@ -43,8 +80,8 @@ class CrawlManga(QWidget):
         self.show()
 
     def initMainLayout(self):
-        self.mainTitle_style = "color: #5176cf; font-weight: bold; font-size: 16pt;"
-        self.title_style = "color: #74ad5c; font-weight: bold; font-size: 10pt;"
+        self.mainTitle_style = common_color["primiary"] + \
+            font["main_title"]+common_font["bold"]
 
         self.mainLayout = QGridLayout()
 
@@ -71,46 +108,65 @@ class CrawlManga(QWidget):
 
         self.tab_rn = QWidget()
         self.initRnLayout()
+        self.tab_dv = QWidget()
+        self.initDvLayout()
         self.tab_gc = QWidget()
 
-        self.tabs.addTab(self.tab_rn, "Rename file")
-        self.tabs.addTab(self.tab_gc, "Getting chapter")
+        self.tabs.addTab(self.tab_rn, tabs["RN"])
+        self.tabs.addTab(self.tab_dv, tabs["DV"])
+        self.tabs.addTab(self.tab_gc, tabs["GC"])
 
     def initRnLayout(self):
+
         self.tab_rn.layout = QGridLayout(self)
 
-        self.RN_lb_main_title = QLabel("Rename file")
+        self.RN_common_str = {
+            "lb_src_folder": "Source folder: ",
+            "btn_get_list": "Get list",
+            "lb_result": "Result file: ",
+            "btn_edit_file": "Edit",
+            "btn_rename": "Rename"
+        }
+
+        self.RN_lb_main_title = QLabel(tabs["RN"])
         self.RN_lb_main_title.setStyleSheet(
-            "color: #198754; font-weight: bold; font-size: 14pt;")
+            common_font["bold"]+common_color["success"]+font["title"])
         self.tab_rn.layout.addWidget(
             self.RN_lb_main_title, 0, 1, alignment=Qt.AlignCenter)
 
-        self.RN_lb_src_folder = QLabel("Source folder")
+        self.RN_lb_src_folder = QLabel(self.RN_common_str["lb_src_folder"])
+        self.RN_lb_src_folder.setStyleSheet(
+            common_font["bold"]+common_color["info"]+font["mini_title"])
         self.tab_rn.layout.addWidget(self.RN_lb_src_folder, 1, 0)
 
         self.RN_tb_src_folder = QLineEdit()
         self.tab_rn.layout.addWidget(self.RN_tb_src_folder, 1, 1)
 
-        self.RN_btn_get_list = QPushButton("Get list")
+        self.RN_btn_get_list = QPushButton(self.RN_common_str["btn_get_list"])
+        self.RN_btn_get_list.setStyleSheet(btns["default"]+btns["primary"])
         self.tab_rn.layout.addWidget(self.RN_btn_get_list, 1, 2)
 
-        self.RN_lb_result = QLabel("Result file")
+        self.RN_lb_result = QLabel(self.RN_common_str["lb_result"])
+        self.RN_lb_result.setStyleSheet(
+            common_font["bold"]+common_color["info"]+font["mini_title"])
         self.tab_rn.layout.addWidget(self.RN_lb_result, 2, 0)
 
         self.RN_lb_result_file = QLabel("")
 
         self.RN_lb_result_file.setStyleSheet(
-            "color: #007bff; text-decoration: underline")
+            common_font["underline"]+common_color["primiary"])
         self.tab_rn.layout.addWidget(self.RN_lb_result_file, 2, 1)
 
-        self.RN_btn_edit_file = QPushButton("Edit")
+        self.RN_btn_edit_file = QPushButton(self.RN_common_str["btn_edit_file"])
+        self.RN_btn_edit_file.setStyleSheet(btns["default"])
         self.tab_rn.layout.addWidget(self.RN_btn_edit_file, 2, 2)
 
         self.RN_progress_rename = QProgressBar()
         self.RN_progress_rename.setValue(0)
         self.tab_rn.layout.addWidget(self.RN_progress_rename, 3, 0, 1, 2)
 
-        self.RN_btn_rename = QPushButton("Rename")
+        self.RN_btn_rename = QPushButton(self.RN_common_str["btn_rename"])
+        self.RN_btn_rename.setStyleSheet(btns["default"] + btns["success"])
         self.tab_rn.layout.addWidget(self.RN_btn_rename, 3, 2)
 
         self.tab_rn.layout.setSpacing(15)
@@ -180,9 +236,38 @@ class CrawlManga(QWidget):
             tmp_progress = int(self.RN_progress * 100 / self.RN_files)
             self.RN_progress_rename.setValue(tmp_progress)
 
-
     def RN_finish_rename(self):
-        n.show_toast('Finish', "Rename folder done!!!", duration=2, threaded=True)
+        n.show_toast(msg["suc_rn"]["t"], msg["suc_rn"]["m"],
+                     duration=2, threaded=True)
+    
+    def initDvLayout(self):
+        self.tab_dv.layout = QGridLayout(self)
+
+        self.DV_common_str = {
+            "lb_link": "Link: ",
+            "lb_result": "Resut file: "
+        }
+
+        self.DV_lb_main_title = QLabel(tabs["DV"])
+        self.DV_lb_main_title.setStyleSheet(
+            common_font["bold"]+common_color["success"]+font["title"])
+        self.tab_dv.layout.addWidget(
+            self.DV_lb_main_title, 0, 0,1,3, alignment=Qt.AlignCenter)
+
+        self.DV_lb_link = QLabel(self.DV_common_str["lb_link"])
+        self.DV_lb_link.setStyleSheet(common_font["bold"]+common_color["info"])
+        self.tab_dv.layout.addWidget(self.DV_lb_link, 1, 0)
+
+        self.DV_tb_link = QLineEdit()
+        self.tab_dv.layout.addWidget(self.DV_tb_link, 1, 1, 1, 2)
+
+        self.DV_lb_result = QLabel(self.DV_common_str["lb_result"])
+        self.DV_lb_result.setStyleSheet(common_font["bold"]+common_color["info"])
+        self.tab_dv.layout.addWidget(self.DV_lb_result, 2,0)
+
+        self.tab_dv.setLayout(self.tab_dv.layout)
+
+
 
 
 def main():
