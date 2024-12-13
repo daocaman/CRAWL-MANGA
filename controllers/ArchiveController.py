472c17efc04b7cff695ec823a7473a8425275095
daocaman <daocaman1998@gmail.com>
2024-12-14 00:09:29 +0700

    update reformat controller

--- controllers/ArchiveController.py
+++ controllers/ArchiveController.py
@@ -13,7 +13,7 @@ def archive_folder(folder='', is_delete=False):
     """
     
     # Debug print initial
-    ARCHIVE_DEBUG and print(Fore.GREEN + '='*70 + Style.RESET_ALL)
+    ARCHIVE_DEBUG and print(Fore.GREEN + '>' + '='*68 + '>' + Style.RESET_ALL)
     ARCHIVE_DEBUG and print(Fore.YELLOW + 'ArchiveController: archive_folder'.center(70) + Style.RESET_ALL)
     ARCHIVE_DEBUG and print(Fore.BLUE + f'{"Folder:":<20}' + Style.RESET_ALL + f'{folder: >49}')
     ARCHIVE_DEBUG and print(Fore.BLUE + f'{"Is delete:":<20}' + Style.RESET_ALL + f'{str(is_delete): >49}')
@@ -30,7 +30,7 @@ def archive_folder(folder='', is_delete=False):
         shutil.rmtree(folder)
 
     # Debug print final
-    ARCHIVE_DEBUG and print(Fore.GREEN + '='*70 + Style.RESET_ALL)
+    ARCHIVE_DEBUG and print(Fore.GREEN + '<' + '='*68 + '<' + Style.RESET_ALL)
 
 def archive_folder_process(process_obj):
     archive_folder(process_obj["folder"], process_obj["is_delete"])
