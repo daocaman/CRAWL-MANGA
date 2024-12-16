4e0209b9f44b23d43776c07ddd00a427bb6853f1
daocaman <daocaman1998@gmail.com>
2024-12-16 16:20:06 +0700

    Fix bug can't rename file

--- tasks/RenameFiles.py
+++ tasks/RenameFiles.py
@@ -3,21 +3,21 @@ import os
 import sys
 from colorama import Fore, Style
 
-from Commons import extract_number, generate_filename, is_image_file
-from Constants import file_prefix, RENAME_DEBUG
+from common.Commons import extract_number, generate_filename, is_image_file
+from common.Constants import file_prefix, RENAME_DEBUG
 
 def main():
     parser = argparse.ArgumentParser(
         description='Rename files')
     parser.add_argument('-o', type=str, required=True, help='target folder')
-    parser.add_argument('-s', type=int, default=0, help='Sort file')
+    parser.add_argument('-s', type=bool, default=False, action='store_true', help='Sort file')
     parser.add_argument('-s_i', type=int, default=0, help='Start index')
-
-    args = parser.parse_args()
 
     if len(sys.argv) == 1:
         parser.print_help()
         sys.exit(1)
+
+    args = parser.parse_args()
 
     if RENAME_DEBUG:
         print(Fore.GREEN + '>' + '='*68 + '>' + Style.RESET_ALL)
@@ -26,12 +26,7 @@ def main():
     try:
         files = os.listdir(args.o)
         files = [f for f in files if is_image_file(f)]
-
-        if args.s == 1:
-            files = sorted(files, key=lambda x: extract_number(x))
-        
-        if args.s == 2:
-            files = sorted(files, key=lambda x: extract_number(x, True))
+        files = sorted(files, key=lambda x: extract_number(x, args.s, args.s))
 
         for i, f in enumerate(files):
             new_name = generate_filename(file_prefix, args.s_i + i, ".jpg")
